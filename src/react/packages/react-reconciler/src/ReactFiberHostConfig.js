176cfb755f02892e4f2673328f070b7a07294d08
danielzhang183 <danielzhang182@gmail.com>
2022-06-26 01:54:12 +0800

    base: export HostConfig

--- src/react/packages/react-reconciler/src/ReactFiberHostConfig.js
+++ src/react/packages/react-reconciler/src/ReactFiberHostConfig.js
@@ -10,6 +10,7 @@
 /* eslint-disable react-internal/invariant-args */
 
 import invariant from 'shared/invariant';
+export * from './forks/ReactFiberHostConfig.dom';
 
 // We expect that our Rollup, Jest, and Flow configurations
 // always shim this module with the corresponding host config
@@ -19,4 +20,4 @@ import invariant from 'shared/invariant';
 // sure that if we *do* accidentally break the configuration,
 // the failure isn't silent.
 
-invariant(false, 'This module must be shimmed by a specific renderer.');
+// invariant(false, 'This module must be shimmed by a specific renderer.');
